d30f616171f9f1c9f2f4106369c565d1c815ef26
Alex Lapinski <contact@alexlapinski.name>
2020-06-20 12:36:01 -0400

    Finished 10-k8s-deployment.sh

--- scripts/10-k8s-deployment.sh
+++ scripts/10-k8s-deployment.sh
@@ -3,10 +3,16 @@
 HOSTNAME=lapinski-k8s-main
 POD_CIDR=10.244.0.0/16
 
+echo "SWAPOFF"
 sudo swapoff -a
 
 # Assign Unique Hostname
+echo "Setting Hostname to $HOSTNAME"
 sudo hostnamectl set-hostname $HOSTNAME
 
 # Initalize K8s on main node
+echo "Initalizing Network with CIDR $POD_CIDR"
 sudo kubeadm init --pod-network-cidr=$POD_CIDR
+
+echo "SWAPON"
+sudo swapon -a
--- scripts/11-k8s-pod-network.sh
+++ scripts/11-k8s-pod-network.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+echo "SWAPOFF"
+sudo swapoff -a
+
+
 sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 
 # Verify
 kubectl get pods --all-namespaces
+
+echo "SWAPON"
+sudo swapon -a
