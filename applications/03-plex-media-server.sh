34ea5d4e93782117f980e2a606487bd5191fa5fc
Alex Lapinski <contact@alexlapinski.name>
2020-06-21 19:59:18 -0400

    Trying to get plex to work

--- applications/03-plex-media-server.sh
+++ applications/03-plex-media-server.sh
@@ -18,3 +18,13 @@ sudo dpkg -i "../binaries/plexmediaserver_$VERSION.deb"
 # TODO: For all external HDs, set their options to
 # include 'nosuid,nodev,nofail'
 #
+# And other tasks from
+# https://askubuntu.com/questions/395291/plex-media-server-wont-find-media-external-hard-drive
+#
+
+sudo chmod go+rx /media/alex
+
+# drives=$(ls /media/alex)
+# TODO: Use variable list
+sudo chmod go+rx /media/alex/terrabyte_drive
+sudo chmod go+rx /media/alex/portable_drive
